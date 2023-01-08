91e20810dca23757f8e9c83491e074e6ac26d0ba
tianheg <yidajiabei@gmail.com>
2023-01-08 14:50:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
+INPUT_ATOMIC=${INPUT_ATOMIC:-true}
 INPUT_FORCE=${INPUT_FORCE:-false}
+INPUT_FORCE_WITH_LEASE=${INPUT_FORCE_WITH_LEASE:-false}
+INPUT_SSH=${INPUT_SSH:-false}
 INPUT_TAGS=${INPUT_TAGS:-false}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
+_ATOMIC_OPTION=''
 _FORCE_OPTION=''
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
@@ -13,8 +17,21 @@ echo "Push to branch $INPUT_BRANCH";
     exit 1;
 };
 
+if ${INPUT_FORCE} && ${INPUT_FORCE_WITH_LEASE}; then
+  echo 'Please, specify only force or force_with_lease and not both.';
+  exit 1;
+fi
+
+if ${INPUT_ATOMIC}; then
+    _ATOMIC_OPTION='--atomic'
+fi
+
 if ${INPUT_FORCE}; then
     _FORCE_OPTION='--force'
+fi
+
+if ${INPUT_FORCE_WITH_LEASE}; then
+    _FORCE_OPTION='--force-with-lease'
 fi
 
 if ${INPUT_TAGS}; then
@@ -23,8 +40,16 @@ fi
 
 cd ${INPUT_DIRECTORY}
 
-remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+if ${INPUT_SSH}; then
+    remote_repo="git@${INPUT_GITHUB_URL}:${REPOSITORY}.git"
+else
+    remote_repo="${INPUT_GITHUB_URL_PROTOCOL}//${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@${INPUT_GITHUB_URL}/${REPOSITORY}.git"
+fi
 
 git config --local --add safe.directory ${INPUT_DIRECTORY}
 
-git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+if ! ${INPUT_FORCE_WITH_LEASE}; then
+  ADDITIONAL_PARAMETERS="${remote_repo} HEAD:${INPUT_BRANCH}"
+fi
+
+git push $ADDITIONAL_PARAMETERS $_ATOMIC_OPTION --follow-tags $_FORCE_OPTION $_TAGS;
